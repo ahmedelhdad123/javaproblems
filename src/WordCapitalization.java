8f53f0ec24caeb98b6ee118b18f1cdf059b55266
AhmedElhdad <91333530+ahmedelhdad123@users.noreply.github.com>
2023-11-04 14:59:54 +0200

    Add files via upload

---
+++ src/WordCapitalization.java
@@ -0,0 +1,21 @@
+
+// Problem  Link    https://codeforces.com/contest/281/problem/A
+
+import java.util.Scanner;
+
+public class WordCapitalization {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String word=scanner.next();
+        if (Character.isUpperCase(word.charAt(0)))
+        {
+            System.out.println(word);
+        }else
+        {
+            StringBuilder stringBuilder = new StringBuilder(word);
+            stringBuilder.setCharAt(0, Character.toUpperCase(word.charAt(0)));
+            String finalWord = stringBuilder.toString();
+            System.out.println(finalWord);
+        }
+    }
+}
