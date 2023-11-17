616bf8f8de5f1d79dac0d808ab0253ddae417551
AhmedElhdad <91333530+ahmedelhdad123@users.noreply.github.com>
2023-11-17 22:01:40 +0200

    Add files via upload

---
+++ src/AlyonaAndNumbers.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class AlyonaAndNumbers {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        long firstNumber,secondNumber;
+        firstNumber=scanner.nextLong();
+        secondNumber=scanner.nextLong();
+        long [] storeF=new long[5];
+        long [] storeS=new long[5];
+        for (int i=1;i<=firstNumber;i++)
+        {
+            storeF[i%5]++;
+        }
+        for (int i=1;i<=secondNumber;i++)
+        {
+            storeS[i%5]++;
+        }
+        System.out.println(storeF[0] * storeS[0] + storeF[1] * storeS[4] + storeF[2] * storeS[3] + storeF[3] * storeS[2] + storeF[4] * storeS[1]);
+    }
+}
