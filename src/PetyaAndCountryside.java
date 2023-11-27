d3ac3afa487c263d32fb31e8b0ed2f90bf6fb6fe
AhmedElhdad <91333530+ahmedelhdad123@users.noreply.github.com>
2023-11-27 16:26:59 +0200

    Add files via upload

---
+++ src/PetyaAndCountryside.java
@@ -0,0 +1,50 @@
+
+/*
+
+ Problem Link
+ https://codeforces.com/contest/66/problem/B
+ */
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class PetyaAndCountryside {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int size=scanner.nextInt();
+        int [] store=new int[size];
+        int counter=1,temp=1;
+        for (int i =0 ;i<size;i++)
+        {
+            store[i]=scanner.nextInt();
+        }
+        for (int i=0;i<size;i++) // 1 2 1 1 1 3 3 4
+        {
+            for (int j=i;j>0;j--)// 1 2 1 1 1 3 3 4
+            {
+                if (store[j]>=store[j-1])
+                {
+                    counter++;
+                }else
+                {
+                    break;
+                }
+            }
+            for (int z=i;z<size-1;z++)
+            {
+                if (store[z]>=store[z+1])
+                {
+                    counter++;
+                }else
+                {
+                    break;
+                }
+            }
+            if (temp<counter)
+            {
+                temp=counter;
+            }
+            counter=1;
+        }
+        System.out.println(temp);
+    }
+}
