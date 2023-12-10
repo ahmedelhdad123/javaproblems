013ce7839e60ed4620ce24a403e2adcaa7327233
AhmedElhdad <91333530+ahmedelhdad123@users.noreply.github.com>
2023-12-10 13:47:47 +0200

    Add files via upload

---
+++ src/LovelyPalindromes.java
@@ -0,0 +1,17 @@
+
+/*
+    Problem Link
+    https://codeforces.com/contest/688/problem/B
+ */
+import java.util.Scanner;
+
+public class LovelyPalindromes {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        String store=scanner.next();
+        StringBuilder copyStore=new StringBuilder(store);
+        copyStore.reverse();
+        String result=store+copyStore;
+        System.out.println(result);
+    }
+}
