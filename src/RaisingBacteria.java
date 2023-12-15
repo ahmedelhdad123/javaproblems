40ed210340532ffa81fbb868bc4b7810c56676b2
AhmedElhdad <91333530+ahmedelhdad123@users.noreply.github.com>
2023-12-15 16:53:53 +0200

    Add files via upload

---
+++ src/RaisingBacteria.java
@@ -0,0 +1,24 @@
+
+/*
+    Problem Link
+      https://codeforces.com/contest/579/problem/A
+ */
+
+import java.util.Scanner;
+
+public class RaisingBacteria {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int bacteria=scanner.nextInt();
+        int result=0;
+        while (bacteria>0)
+        {
+            if (bacteria%2==1)
+            {
+                result++;
+            }
+            bacteria /=2;
+        }
+        System.out.println(result);
+    }
+}
