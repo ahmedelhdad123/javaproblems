2bf1326b80b17fd50b75f8bbcc5efc673ba4073f
AhmedElhdad <91333530+ahmedelhdad123@users.noreply.github.com>
2023-12-20 15:59:24 +0200

    Add files via upload

---
+++ src/NumbersHistogram.java
@@ -0,0 +1,23 @@
+
+/*
+   Problem LInk
+   https://codeforces.com/group/MWSDmqGsZm/contest/219432/problem/N
+ */
+import java.util.Scanner;
+
+public class NumbersHistogram {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        char shape=scanner.next().charAt(0);
+        int size=scanner.nextInt();
+        while (size-->0)
+        {
+            int number=scanner.nextInt();
+            for (int i=0;i<number;i++)
+            {
+                System.out.print(shape);
+            }
+            System.out.println();
+        }
+    }
+}
