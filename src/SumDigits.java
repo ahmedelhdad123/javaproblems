3f33f043f32821353a079d2911b06d220d30a6d3
AhmedElhdad <91333530+ahmedelhdad123@users.noreply.github.com>
2023-12-22 17:53:10 +0200

    Add files via upload

---
+++ src/SumDigits.java
@@ -0,0 +1,22 @@
+
+/*
+    Problem Link
+   https://codeforces.com/group/MWSDmqGsZm/contest/219774/problem/K
+ */
+import java.util.Scanner;
+
+public class SumDigits {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int size=scanner.nextInt();
+        String number=scanner.next();
+        int sum=0;
+        int store[] =new int[size];
+        for (int i=0;i<size;i++)
+        {
+            store[i]=number.charAt(i)-'0';
+            sum+=store[i];
+        }
+        System.out.println(sum);
+    }
+}
