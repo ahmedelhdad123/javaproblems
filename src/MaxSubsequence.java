e3481d053307838da5db8c6be905d19a5b3af336
ahmedelhdad123 <ahmedelhdad956@gmail.com>
2024-01-01 14:12:11 +0200

    1/1 Task Closes #5

---
+++ src/MaxSubsequence.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class MaxSubsequence {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int size = scanner.nextInt();
+        String store = scanner.next();
+        String check= String.valueOf(store.charAt(0));
+       for (int i=0;i<size-1;i++)
+       {
+           if (store.charAt(i)!=store.charAt(i+1))
+           {
+               check=check+store.charAt(i);
+           }
+       }
+        System.out.println(check.length());
+    }
+}
