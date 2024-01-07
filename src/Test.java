dfda718d7e2325e26900f339a45607d969d151da
ahmedelhdad123 <ahmedelhdad956@gmail.com>
2024-01-07 17:30:45 +0200

    6/1 Task Closes #10

---
+++ src/Test.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        String a="ahmed";
+        StringBuilder b=new StringBuilder(a);
+        b.append(b.charAt(3));
+        a=b.toString();
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
