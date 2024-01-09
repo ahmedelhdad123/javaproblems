52938a08c44cf275d81dbdb683255c2748076c61
ahmedelhdad123 <ahmedelhdad956@gmail.com>
2024-01-09 14:50:57 +0200

    9/1 Task #13

---
+++ src/PrimeFunction.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class PrimeFunction {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int size=scanner.nextInt();
+        while (size-->0)
+        {
+            int number=scanner.nextInt();
+            System.out.println(isPrime(number));
+        }
+    }
+    public static String isPrime(int number)
+    {
+        if (number <= 1) {
+            return "NO";
+        }
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return "NO";
+            }
+        }
+        return "YES";
+    }
+}
