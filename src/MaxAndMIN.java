4073835735c31aec92c0c0338c14fb9e659881ff
ahmedelhdad123 <ahmedelhdad956@gmail.com>
2024-01-11 12:57:33 +0200

    11/1 Task #15

---
+++ src/MaxAndMIN.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class MaxAndMIN {
+    public static void main(String[] args) {
+
+        Scanner scanner=new Scanner(System.in);
+        int size=scanner.nextInt();
+        int [] store=new int[size];
+        for (int i = 0; i < size; i++) {
+            store[i] = scanner.nextInt();
+        }
+        System.out.println(Min(store) + " " + Max(store));
+    }
+
+    private static int Max(int[] store) {
+        int temp=Integer.MIN_VALUE;
+        for (int i=0;i<store.length;i++)// 10 13 95 1 3
+        {
+            if (temp<store[i])
+            {
+                temp=store[i];
+            }
+        }
+        return temp;
+    }
+
+    private static int Min(int[] store) {
+        int temp=Integer.MAX_VALUE;
+        for (int i=0;i<store.length;i++)// 10 13 95 1 3
+        {
+            if (temp>store[i])
+            {
+                temp=store[i];
+            }
+        }
+        return temp;
+    }
+}
