9747b0abfea48fff959da7b2fe4306b8ff0b64d1
ahmedelhdad123 <ahmedelhdad956@gmail.com>
2024-01-26 14:00:23 +0200

    26/1 Task Closes #27

---
+++ src/PrimeFactors.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class PrimeFactors {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        long num=scanner.nextLong();
+        int flag=0;
+        for (int i=2; i<=num/2; i++) {
+            int counter=0;
+            while (num%i==0) {
+                counter++;
+                num=num/i;
+            }
+            if (counter>0&&flag==1)
+            {
+                System.out.print("*");
+            }
+            if (counter>0)
+            {
+                System.out.print("("+i+"^"+counter+")");
+                flag=1;
+            }
+            if (num==1)
+            {
+                return;
+            }
+        }
+        if (num>1 && flag==1)
+        {
+            System.out.print("*");
+        }
+        if (num>0)
+        {
+            System.out.print("("+num+"^"+1+")");
+        }
+    }
+}
