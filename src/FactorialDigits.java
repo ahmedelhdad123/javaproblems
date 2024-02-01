be468e4ef8c3d04930bde400c3b6da4abd3ec586
ahmedelhdad123 <ahmedelhdad956@gmail.com>
2024-02-01 14:03:34 +0200

    1/2 Task Closes #33

---
+++ src/FactorialDigits.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class FactorialDigits {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            double num = scanner.nextDouble();
+            double result = 0;
+
+            for (int i = 2; i <= num; i++) {
+                result += Math.log10(i);
+            }
+
+            System.out.println("Number of digits of " + (int)num + "! is " + ((int) result + 1));
+        }
+}
