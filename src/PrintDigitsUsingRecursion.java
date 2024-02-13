d6e70726d34b75df7ed2310c55263e48311acd1b
ahmedelhdad123 <ahmedelhdad956@gmail.com>
2024-02-13 11:44:08 +0200

    13/2 Task Closes #45

---
+++ src/PrintDigitsUsingRecursion.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class PrintDigitsUsingRecursion {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int n=scanner.nextInt();
+        while (n-->0)
+        {
+            String store=scanner.next();
+            printDigitsUsingRecursion(store);
+            System.out.println();
+
+        }
+    }
+
+    private static void printDigitsUsingRecursion(String store) {
+        if (store.isEmpty()){
+            return;
+        }
+        System.out.print(store.charAt(0)+" ");
+        printDigitsUsingRecursion(store.substring(1));
+
+    }
+}
