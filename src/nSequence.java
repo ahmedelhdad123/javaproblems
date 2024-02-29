b6098455e52367f5dcc16ee965799f126c1cd46c
ahmedelhdad123 <ahmedelhdad956@gmail.com>
2024-02-29 12:36:29 +0200

    29/2 Task Closes #56

---
+++ src/nSequence.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class nSequence {
+    static long count=1;
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        long number=scanner.nextLong();
+        NSequence(number);
+        System.out.println(count);
+    }
+
+    private static long NSequence(long number) {
+        if (number == 1 )
+        {
+            return count;
+        }
+        if (number%2==0)
+        {
+            number=number/2;
+            count++;
+        }else
+        {
+            number=3*number+1;
+            count++;
+        }
+        return NSequence(number);
+    }
+}
