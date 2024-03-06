51f1341b75f9efde89a9a641adb8a2bb0029cd97
ahmedelhdad123 <ahmedelhdad956@gmail.com>
2024-03-06 11:57:40 +0200

    6/3 Task Closes #62

---
+++ src/ReachValue.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class ReachValue {
+    static long input;
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int number=scanner.nextInt();
+        while (number-->0)
+        {
+            input=scanner.nextLong();
+            if (reachValue(1))
+            {
+                System.out.println("YES");
+            }else
+            {
+                System.out.println("NO");
+            }
+        }
+
+    }
+
+    private static boolean reachValue(long number) {
+        if (number>input)
+        {
+            return false;
+        }
+        if (number==input)
+        {
+            return true;
+        }else
+        {
+            return reachValue(number*10)||reachValue(number*20);
+        }
+    }
+}
