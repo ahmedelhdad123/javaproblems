ccb0c3805ac03154b9793c15a8fd94fe7da83c52
ahmedelhdad123 <ahmedelhdad956@gmail.com>
2024-03-27 16:17:11 +0200

    27/3 Task Closes #68

---
+++ src/KokoAndTheTransformation.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class KokoAndTheTransformation {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int numOfArray1=scanner.nextInt();
+        int numOfArray2=scanner.nextInt();
+        int sum1=0,sum2=0;
+        int [] array1=new int[numOfArray1];
+        int [] array2=new int[numOfArray2];
+        for (int i=0;i<numOfArray1;i++){
+            array1[i]=scanner.nextInt();
+            sum1+=array1[i];
+        }
+        for (int i=0;i<numOfArray2;i++){
+            array2[i]=scanner.nextInt();
+            sum2+=array2[i];
+        }
+        System.out.println(sum1==sum2?"Yes":"No");
+
+    }
+}
