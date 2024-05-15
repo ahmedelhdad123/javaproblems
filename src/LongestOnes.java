90a9c2433f1a03f231c33d405c239db85ec5518c
ahmedelhdad123 <ahmedelhdad956@gmail.com>
2024-05-15 20:25:11 +0300

    15/5 Task Closes #83

---
+++ src/LongestOnes.java
@@ -0,0 +1,35 @@
+
+/*
+  LeetCode ProblemLink
+  https://leetcode.com/problems/max-consecutive-ones-iii/submissions/1257771660/?envType=study-plan-v2&envId=leetcode-75
+ */
+import java.util.Scanner;
+
+public class LongestOnes {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int[] nums = new int[size];
+        for (int i = 0; i < size; i++) {
+            nums[i] = sc.nextInt();
+        }
+        int k = sc.nextInt();
+        int max = Integer.MIN_VALUE;
+
+        for (int i = 0; i < nums.length; i++) {
+            int count = 0;
+            for (int j = i, flips = k; j < nums.length; j++) {
+                if (nums[j] == 1) {
+                    count++;
+                } else if (flips > 0) {
+                    count++;
+                    flips--;
+                } else {
+                    break;
+                }
+            }
+            max = Math.max(max, count);
+        }
+        System.out.println(max);
+    }
+}
---
+++ src/LongestSubarray.java
@@ -0,0 +1,39 @@
+
+/*
+   LeetCode Problem Link
+   https://leetcode.com/problems/longest-subarray-of-1s-after-deleting-one-element/?envType=study-plan-v2&envId=leetcode-75
+ */
+import java.util.Scanner;
+
+public class LongestSubarray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+        int max = Integer.MIN_VALUE;
+
+        for (int i = 0; i < nums.length; i++) {
+            int count = 0;
+            for (int j = i,k=1; j < nums.length; j++) {
+                if (nums[j]==1)
+                {
+                    count++;
+                } else if (nums[j]==0&&k==1) {
+                    k--;
+                }else
+                {
+                    break;
+                }
+            }
+            max = Math.max(max,count);
+        }
+        if (max==nums.length)
+        {
+            max--;
+        }
+        System.out.println(max);
+    }
+}
